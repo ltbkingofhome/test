805f0137e5085d9a33c8d3cade8cd1c771743794
l <l@google.com>
2023-11-08 20:33:40 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
 #![allow(unused)]
 fn main() {
-let string: str = "banana";
+let s1: &str = "banana";
+let s2: &str = &String::from("banana");
+
+let arr = [1, 2, 3, 4, 5];
+
+let s3: &[i32] = &arr[1..3];
 }
