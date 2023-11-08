b1f080b6cd6a60fcf671459b8f777184dbb8b0ec
l <l@google.com>
2023-11-08 20:42:40 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-#![allow(unused)]
 fn main() {
-let s1: &str = "banana";
-let s2: &str = &String::from("banana");
-
-let arr = [1, 2, 3, 4, 5];
-
-let s3: &[i32] = &arr[1..3];
-}
+    let my_name = "Pascal";
+    greet(my_name);
+  }
+  
+  fn greet(name: String) {
+    println!("Hello, {}!", name);
+  }
